72d4aaf5401a0b5f7592be53e7066ed7169ff03f
dhalmazna <dhalmazna@seznam.cz>
2026-02-26 07:48:35 +0100

    chore: uncomment CIAOExplainer from __init__

--- ciao/__init__.py
+++ ciao/__init__.py
@@ -4,4 +4,7 @@ An explainable AI (XAI) method for identifying influential image regions using
 Mutual Information and greedy feature selection.
 """
 
-# from ciao.explainer.ciao_explainer import CIAOExplainer
+from ciao.explainer.ciao_explainer import CIAOExplainer
+
+
+__all__ = ["CIAOExplainer"]
