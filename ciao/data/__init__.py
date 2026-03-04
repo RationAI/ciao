b3d7572dc25e9da05950a386dc22de09bb46120b
dhalmazna <dhalmazna@seznam.cz>
2026-03-04 16:35:38 +0100

    feat: add image preprocessing function

--- ciao/data/__init__.py
+++ ciao/data/__init__.py
@@ -1,6 +1,7 @@
 """Data loading utilities for CIAO."""
 
 from ciao.data.loader import get_image_loader
+from ciao.data.preprocessing import load_and_preprocess_image
 
 
-__all__ = ["get_image_loader"]
+__all__ = ["get_image_loader", "load_and_preprocess_image"]
