de00a02b3198c9c15394573e60f4c6d29ac2d4b3
dhalmazna <dhalmazna@seznam.cz>
2026-03-05 11:18:35 +0100

    chore: update __init__.py

--- ciao/data/__init__.py
+++ ciao/data/__init__.py
@@ -2,7 +2,19 @@
 
 from ciao.data.loader import get_image_loader
 from ciao.data.preprocessing import load_and_preprocess_image
+from ciao.data.replacement import (
+    calculate_image_mean_color,
+    get_replacement_image,
+    plot_image_mean_color,
+)
 from ciao.data.segmentation import create_segmentation
 
 
-__all__ = ["create_segmentation", "get_image_loader", "load_and_preprocess_image"]
+__all__ = [
+    "calculate_image_mean_color",
+    "create_segmentation",
+    "get_image_loader",
+    "get_replacement_image",
+    "load_and_preprocess_image",
+    "plot_image_mean_color",
+]
