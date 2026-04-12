fa191a0fae9f5d9402ad5a6e3e4bbf10df56b0a7
dhalmazna <dhalmazna@seznam.cz>
2026-04-12 15:25:21 +0200

    feat: implement potential algorithm using Sequential Monte Carlo with potential-based selection

--- ciao/algorithm/__init__.py
+++ ciao/algorithm/__init__.py
@@ -4,6 +4,7 @@ from ciao.algorithm.builder import build_all_regions
 from ciao.algorithm.context import SearchContext
 from ciao.algorithm.graph import ImageGraph
 from ciao.algorithm.lookahead import build_region_greedy_lookahead
+from ciao.algorithm.potential import build_region_potential
 from ciao.algorithm.search_helpers import is_terminal
 
 
@@ -12,5 +13,6 @@ __all__ = [
     "SearchContext",
     "build_all_regions",
     "build_region_greedy_lookahead",
+    "build_region_potential",
     "is_terminal",
 ]
