63b1ea8b193d6773ebcc9e5b70e3d31b3118b964
dhalmazna <dhalmazna@seznam.cz>
2026-05-02 09:19:27 +0200

    feat: add visualization of deletion curve and saliency map

--- ciao/metrics/__init__.py
+++ ciao/metrics/__init__.py
@@ -3,6 +3,7 @@
 from ciao.metrics.saliency import (
     build_saliency_map,
     compute_deletion_auc,
+    compute_deletion_curve,
     compute_pointing_game,
 )
 from ciao.metrics.segmentation import compute_iou
@@ -11,6 +12,7 @@ from ciao.metrics.segmentation import compute_iou
 __all__ = [
     "build_saliency_map",
     "compute_deletion_auc",
+    "compute_deletion_curve",
     "compute_iou",
     "compute_pointing_game",
 ]
