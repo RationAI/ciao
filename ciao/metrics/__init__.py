0e7b490c93b63d7b9e0f40fac907ed66e8924c73
dhalmazna <dhalmazna@seznam.cz>
2026-05-02 08:41:46 +0200

    feat: add saliency and deletion auc

--- ciao/metrics/__init__.py
+++ ciao/metrics/__init__.py
@@ -1,6 +1,7 @@
 """Evaluation metrics for CIAO explanations."""
 
+from ciao.metrics.deletion import build_saliency_map, compute_deletion_auc
 from ciao.metrics.segmentation import compute_iou
 
 
-__all__ = ["compute_iou"]
+__all__ = ["build_saliency_map", "compute_deletion_auc", "compute_iou"]
