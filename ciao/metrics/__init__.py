97d99ca6c4512f0cef26cffd594177d2459644f2
dhalmazna <dhalmazna@seznam.cz>
2026-05-02 09:00:15 +0200

    feat: add pointing game

--- ciao/metrics/__init__.py
+++ ciao/metrics/__init__.py
@@ -1,7 +1,16 @@
 """Evaluation metrics for CIAO explanations."""
 
-from ciao.metrics.deletion import build_saliency_map, compute_deletion_auc
+from ciao.metrics.saliency import (
+    build_saliency_map,
+    compute_deletion_auc,
+    compute_pointing_game,
+)
 from ciao.metrics.segmentation import compute_iou
 
 
-__all__ = ["build_saliency_map", "compute_deletion_auc", "compute_iou"]
+__all__ = [
+    "build_saliency_map",
+    "compute_deletion_auc",
+    "compute_iou",
+    "compute_pointing_game",
+]
